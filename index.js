db650a190d92b77a1ad44b25cdb91debc6a5e7c6
vincent <vincent.bertin@epitech.eu>
2018-01-21 14:26:21 +0100

    Works fine... For me.

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-var docker = require('./lib/docker');
+var docker 	= require('./lib/docker');
 var project = require('./lib/project');
-var args = require('./lib/args');
+var args 	= require('./lib/args');
+var opts 	= require('./lib/options');
 
+opts.epidockerDir = __dirname;
 args.init();
