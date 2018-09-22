68ff6169c2a15c63d60c2c247e0294a5b9b723ad
vincent <vincent.bertin@epitech.eu>
2018-09-22 15:21:11 +0200

    Clean up

--- index.js
+++
@@ -1,7 +0,0 @@
-var docker 	= require('./lib/docker');
-var project = require('./lib/project');
-var args 	= require('./lib/args');
-var opts 	= require('./lib/options');
-
-opts.epidockerDir = __dirname;
-args.init();
